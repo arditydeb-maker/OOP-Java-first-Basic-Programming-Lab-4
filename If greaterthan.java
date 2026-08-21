175876b63c4c482883f5da8bd9ac57edab7f94bf
arditydeb-maker <arditydeb@gmail.com>
2026-08-21 17:26:39 +0600

    Update If greaterthan.java

--- If greaterthan.java
+++ If greaterthan.java
@@ -1,3 +1,7 @@
-if (20 > 18) {
-  System.out.println("20 is greater than 18");
+public class Main {
+  public static void main(String[] args) {
+    if (20 > 18) {
+      System.out.println("20 is greater than 18"); // obviously
+    }  
+  }
 }
